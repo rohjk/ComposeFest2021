986d45e62c9e9ddebd6c340dc9669391960e7cba
노정근 <rohjk93@kpu.ac.kr>
2021-11-18 13:43:06 +0900

    step7 Create your custom layout

--- week 2-1-Layouts in JetpackCompose/ComposeApp/app/src/main/java/com/jk/composeapp/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/ComposeApp/app/src/main/java/com/jk/composeapp/MainActivity.kt
@@ -34,8 +34,13 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.jk.composeapp.ui.theme.ComposeAppTheme
@@ -46,7 +51,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeAppTheme {
-                ImageList()
+                LayoutsCodelab()
             }
         }
     }
@@ -135,8 +140,14 @@ fun BodyContent(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.padding(8.dp)
     ) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
+        MyOwnColumn(
+            modifier = modifier.padding(8.dp)
+        ) {
+            Text("MyOwnColumn")
+            Text("places items")
+            Text("vertically.")
+            Text("We've done it by hand!")
+        }
     }
 }
 
@@ -212,5 +223,70 @@ fun ImageListItem(index: Int) {
 fun ImageListPreview() {
     ComposeAppTheme {
         ImageList()
+    }
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaseLineToTop: Dp,
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints = constraints)
+
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseLine = placeable[FirstBaseline]
+
+        val placeableY = firstBaseLineToTop.roundToPx() - firstBaseLine
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    ComposeAppTheme {
+        Text(
+            text = "Hi there!",
+            modifier = Modifier.firstBaselineToTop(32.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TextWithMormalPaddingPreview() {
+    ComposeAppTheme {
+        Text(
+            text = "Hi there!",
+            modifier = Modifier.padding(top = 32.dp)
+        )
+    }
+}
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content,
+    ) { measurables, constraints ->
+        val placeable = measurables.map { measurable ->
+            measurable.measure(constraints = constraints)
+        }
+        var yPosition = 0
+
+        layout(
+            constraints.maxWidth,
+            constraints.maxHeight
+        ) {
+            placeable.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+                yPosition += placeable.height
+            }
+        }
     }
 }
